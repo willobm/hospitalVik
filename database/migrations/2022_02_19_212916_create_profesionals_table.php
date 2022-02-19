7dfac73c69e4aafcc60125c8491cd4c60321c4d9
viktorengel <viktorengel@hotmail.com>
2022-02-19 17:02:11 -0500

    Creada la base de datos y sus respectivas relaciones a nivel de base

--- database/migrations/2022_02_19_212916_create_profesionals_table.php
+++ database/migrations/2022_02_19_212916_create_profesionals_table.php
@@ -13,16 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('profesionals', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->foreignId('current_team_id')->nullable();
-            $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
+
+            $table->unsignedBigInteger('persona_id');
+            $table->foreign('persona_id')->references('id')->on('personas')->onDelete('cascade');
+            $table->unsignedBigInteger('especialidad_id');
+            $table->foreign('especialidad_id')->references('id')->on('especialidads')->onDelete('cascade');
         });
     }
 
@@ -33,7 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('profesionals');
     }
-
 };
--- database/migrations/2022_02_19_213401_create_examens_table.php
+++ database/migrations/2022_02_19_213401_create_examens_table.php
@@ -13,16 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('examens', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->foreignId('current_team_id')->nullable();
-            $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
+
+            $table->unsignedBigInteger('folio_id');
+            $table->foreign('folio_id')->references('id')->on('folios')->onDelete('cascade');
         });
     }
 
@@ -33,7 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('examens');
     }
-
 };
